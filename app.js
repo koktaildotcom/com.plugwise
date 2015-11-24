c89e9402d19845eb0aae363f9b473e3a5f2d3267
Freek Boelders <freek@athom.nl>
2015-11-24 11:51:40 +0100

    Changed app structure

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-/* global Homey */
 "use strict";
-  
+
 var mdns = require('mdns-js');
 var browser = mdns.createBrowser(mdns.tcp("plugwise"));
 var devices = [];
 
-var App = module.exports;
+var self = module.exports = {
 
-App.init = function(){
-	browser.on('ready', function () {
-		browser.discover();
-	});
+	init: function () {
+		browser.on('ready', function () {
+			browser.discover();
+		});
 
-	browser.on('update', function (data) {
-		devices.push(data);
-	});
-}
+		browser.on('update', function (data) {
+			devices.push(data);
+		});
+	},
 
-App.getDevices = function(device_type, callback){
-	setTimeout(function() {
-		return callback(devices.filter(function(x) { return x.txt[0].indexOf(device_type) > -1 }));
-	}, 10000);
+	getDevices: function() {
+		setTimeout(function() {
+			return callback(devices.filter(function(x) { return x.txt[0].indexOf(device_type) > -1 }));
+		}, 10000);
+	}
 }
